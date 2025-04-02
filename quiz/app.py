a63c72a7d8445b31754cbce22064a986d2ef37e0
Yasin <keskan543@gmail.com>
2025-04-02 16:46:11 +0200

    azaz

--- quiz/app.py
+++ quiz/app.py
@@ -1,26 +1,23 @@
 import os.path
 
 from flask import Flask
+from .extensions import db, migrate
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 
-app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origins": "*"}})
-
 def mkpath(p):
-    return os.path.normpath(
-        os.path.join(
-            os.path.dirname(__file__), p
-        )
-    )
+    return os.path.normpath(os.path.join(os.path.dirname(__file__), p))
 
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    'sqlite:///'+mkpath('../quiz.db')
-)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + mkpath('../quiz.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+migrate.init_app(app, db)
+
+from . import models
 
 with app.app_context():
     db.create_all()
--- quiz/models.py
+++ quiz/models.py
@@ -1,5 +1,5 @@
 from .app import db
-
+from .extensions import db
 
 class Questionnaire(db.Model):
     id = db.Column(db.Integer, primary_key=True)
